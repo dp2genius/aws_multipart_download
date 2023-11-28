c985a712275f39277dcf13e98ba6b66ddb40b082
dp2genius <dp2genius@gmail.com>
2023-11-28 03:39:44 -0500

    fix: use react typescript

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+  credentials: {
+    accessKeyId: "AKIAVFN47PAUJIJ4TNPO",
+    secretAccessKey: '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',
+  },
+  Bucket: "skand.development-kevin.temporary-file-upload",
+  Key: 'Colttaine.zip',
+  region: "ap-southeast-2",
+};
